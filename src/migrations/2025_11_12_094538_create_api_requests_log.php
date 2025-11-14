518bf546a9c1661440f076e896885b9280038d30
test_local_xh <axhiny@163.com>
2025-11-14 11:25:55 +0800

    xh

--- src/migrations/2025_11_12_094538_create_api_requests_log.php
+++ src/migrations/2025_11_12_094538_create_api_requests_log.php
@@ -26,15 +26,15 @@ class CreateApiRequestsLog extends Migration
     {
         Schema::create(\Qscmf\HttpTracer\Lib\MigrationCommon::getTableName(false), function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('trace_id', 32);
-            $table->string('method', 10);
-            $table->text('url');
-            $table->text('request_headers');
-            $table->text('request_body')->default(null)->nullable(true);
-            $table->integer('response_status_code')->default(null)->nullable(true);
-            $table->text('response_headers')->default(null)->nullable(true);
-            $table->text('response_body')->default(null)->nullable(true);
-            $table->float('duration_ms')->default(null)->nullable(true);
+            $table->string('trace_id', 32)->comment("@title=追踪ID;@require=true;");
+            $table->string('method', 10)->comment("@title=请求方法;@require=true;");
+            $table->text('url')->comment("@title=请求URL;@require=true;");
+            $table->text('request_headers')->comment("@title=请求头;@require=true;");
+            $table->text('request_body')->default(null)->nullable(true)->comment("@title=请求体;");
+            $table->integer('response_status_code')->default(null)->nullable(true)->comment("@title=响应状态码;");
+            $table->text('response_headers')->default(null)->nullable(true)->comment("@title=响应头;");
+            $table->text('response_body')->default(null)->nullable(true)->comment("@title=响应体;");
+            $table->float('duration_ms')->default(null)->nullable(true)->comment("@title=请求耗时(毫秒);");
             $table->timestamp('update_date', 4)->default(DB::raw('CURRENT_TIMESTAMP(4) ON UPDATE CURRENT_TIMESTAMP(4)'));
             $table->timestamp('create_date', 4)->default(DB::raw('CURRENT_TIMESTAMP(4)'));
         });
